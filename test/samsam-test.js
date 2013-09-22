0f8331730cc6c3e7ca09af63f1ff9806d9da2a38
Christian Johansen <christian@cjohansen.no>
2013-09-22 12:48:11 +0200

    Fix bug where match([], []) failed

--- test/samsam-test.js
+++ test/samsam-test.js
@@ -337,6 +337,8 @@ if (typeof module === "object" && typeof require === "function") {
         pass("matching array subset", [1, 2, 3, { id: 42 }], [{ id: 42 }]);
         fail("mis-matching array 'subset'", [1, 2, 3], [2, 3, 4]);
         fail("mis-ordered array 'subset'", [1, 2, 3], [1, 3]);
+        pass("empty arrays", [], []);
+        pass("objects with empty arrays", { xs: [] }, { xs: [] });
     });
 
 }());
