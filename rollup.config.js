2433ce0f126f5fcac16c5de52568802f5cd8ce04
Morgan Roderick <morgan@roderick.dk>
2018-03-19 08:30:18 +0100

    Remove UMD wrapper from source

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var commonjs = require("rollup-plugin-commonjs");
+
+module.exports = {
+    entry: "lib/samsam.js",
+    exports: "named",
+    format: "umd",
+    moduleName: "samsam",
+    plugins: [
+        commonjs({sourceMap: false})
+    ]
+};
