sinonjs/samsam

commit 31b4e43b600fa6e8cad6444516f420966b0866ee

feat(deepEqual): compare constructor name of objects (#38)
This feature adds the comparision of the `name`
property of a given objects constructor if there
is any.

Otherwise, an empty object ({}) will be equal an
empty class instance (new class MyClass {}).

See #37

lib/get-class-name.js
@@ -1,0 +1,7 @@
+"use strict";
+
+function getClassName(value) {
+    return Object.getPrototypeOf(value) ? value.constructor.name : null;
+}
+
+module.exports = getClassName;

lib/deep-equal.js
@@ -9,6 +9,7 @@ var isNaN = require("./is-nan");
 var isObject = require("./is-object");
 var isSet = require("./is-set");
 var isSubset = require("./is-subset");
+var getClassName = require("./get-class-name");
 
 var every = Array.prototype.every;
 var getTime = Date.prototype.getTime;
@@ -96,6 +97,8 @@ function deepEqualCyclic(first, second) {
         var class2 = getClass(obj2);
         var keys1 = keys(obj1);
         var keys2 = keys(obj2);
+        var name1 = getClassName(obj1);
+        var name2 = getClassName(obj2);
 
         if (isArguments(obj1) || isArguments(obj2)) {
             if (obj1.length !== obj2.length) {
@@ -105,7 +108,8 @@ function deepEqualCyclic(first, second) {
             if (
                 type1 !== type2 ||
                 class1 !== class2 ||
-                keys1.length !== keys2.length
+                keys1.length !== keys2.length ||
+                (name1 && name2 && name1 !== name2)
             ) {
                 return false;
             }

lib/deep-equal.test.js
@@ -414,6 +414,15 @@ describe("deepEqual", function() {
         assert.isTrue(samsam.deepEqual(element, element));
     });
 
+    it("return false if empty object to empty instance", function() {
+        // Because eslint-config-sinon disables es6, we can't
+        // use a class definition here
+        // https://github.com/sinonjs/eslint-config-sinon/blob/master/index.js
+        // var instance = new (class TestClass {});
+        var instance = new function TestClass() {}();
+        assert.isFalse(samsam.deepEqual(obj, instance));
+    });
+
     it("fails different DOM elements", function() {
         var element = document.createElement("div");
         var el = document.createElement("div");

lib/get-class-name.test.js
@@ -1,0 +1,27 @@
+"use strict";
+
+var assert = require("@sinonjs/referee").assert;
+var getClassName = require("./get-class-name");
+
+describe("getClassName", function() {
+    it("returns the class name of an instance", function() {
+        // Because eslint-config-sinon disables es6, we can't
+        // use a class definition here
+        // https://github.com/sinonjs/eslint-config-sinon/blob/master/index.js
+        // var instance = new (class TestClass {})();
+        var instance = new function TestClass() {}();
+        var name = getClassName(instance);
+        assert.equals(name, "TestClass");
+    });
+
+    it("returns 'Object' for {}", function() {
+        var name = getClassName({});
+        assert.equals(name, "Object");
+    });
+
+    it("returns null for an object that has no prototype", function() {
+        var obj = Object.create(null);
+        var name = getClassName(obj);
+        assert.equals(name, null);
+    });
+});

commit 59ed406b0f80503bd82e06fff84facb1035652d2

Fix failing Class tests in IE11
These were introduced in https://github.com/sinonjs/samsam/pull/38

lib/get-class-name.js
@@ -1,7 +1,20 @@
 "use strict";
 
+var re = /function (\w+)\s*\(/;
+
 function getClassName(value) {
-    return value.constructor ? value.constructor.name : null;
+    if (value.constructor && "name" in value.constructor) {
+        return value.constructor.name;
+    }
+
+    if (typeof value.constructor === "function") {
+        var match = value.constructor.toString().match(re);
+        if (match.length > 1) {
+            return match[1];
+        }
+    }
+
+    return null;
 }
 
 module.exports = getClassName;